f5e232bbde85c72da18a56a094d76850d420fea0
Kamalesh G <148445018+kamalesh2602@users.noreply.github.com>
2026-07-29 23:54:59 +0530

    Update resume link to Google Drive

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -3,5 +3,5 @@ export const socials = {
   github: "https://github.com/kamalesh2602/",
   linkedin: "https://www.linkedin.com/in/kamaleshg2602/",
   email: "kamaleshg2602@gmail.com",
-  resume: "/resume/Kamalesh.pdf",
+  resume: "https://drive.google.com/file/d/1LlEUhW8gFCgdp5uMhXpC-lsngiynOkgS/view?usp=drive_link",
 };
