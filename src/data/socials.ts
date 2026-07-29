fa952c43a6b26d3f3adcf0ead0da7730bece1bf2
Kamalesh G <148445018+kamalesh2602@users.noreply.github.com>
2026-07-29 22:51:22 +0530

    Update LinkedIn URL in socials data

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,7 +1,7 @@
 
 export const socials = {
   github: "https://github.com/kamalesh2602/",
-  linkedin: "https://www.linkedin.com/in/kamalesh-g-4593633a1/",
+  linkedin: "https://www.linkedin.com/in/kamaleshg2602/",
   email: "kamaleshg2602@gmail.com",
   resume: "/resume/Kamalesh.pdf",
 };
